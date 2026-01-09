74ca52bd14835b7e2349bde6948ed3278197e770
shiburito <rob+shibadev@projectshiba.com>
2026-01-09 00:23:58 -0600

    add in twitter feed add docker-compose for dev upated env example

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require 'discordrb'
 require 'dotenv/load'
 require 'sinatra'
+require 'redis'
 
 require_relative 'commands/favor_command'
+require_relative 'lib/twitter_feed'
 
+commands = [FavorCommand.new]
 server_id = ENV.fetch('DISCORD_SERVER_ID', nil)
 bot = Discordrb::Bot.new(token: ENV.fetch('DISCORD_BOT_TOKEN', nil), intents: :all)
-commands = [FavorCommand.new]
+redis_config = RedisClient.config(url: ENV['REDIS_URL'])
+redis_client = redis_config.new_pool(timeout: 0.5, size: Integer(ENV.fetch("REDIS_MAX_THREADS", 2)))
+twitter_feed_frequency_seconds = ENV['TWITTER_FEED_FREQUENCY_SECONDS']
+
+if redis_client.call("PING") != 'PONG'
+  puts("Error contacting redis, check that it's up and accessible!")
+  exit 1
+end
 
 commands.each do |command|
   bot.register_application_command(command.id, command.description, server_id: server_id) do |cmd|
@@ -40,6 +50,20 @@ commands.each do |command|
 end
 
 bot.run true
+
+twitter_feed = DevonaBot::TwitterFeed.new(bot, redis_client)
+
+Thread.new do
+  loop do
+    begin
+      twitter_feed.process
+      sleep(twitter_feed_frequency_seconds.to_i)
+    rescue => e
+      puts "There was an error processing the twitter rss feed #{e}"
+      sleep(2)
+    end
+  end
+end
 
 get '/' do
   'Hello'
---
+++ lib/twitter_feed.rb
@@ -0,0 +1,54 @@
+require 'rss'
+require 'discordrb'
+
+module DevonaBot
+  class TwitterFeed
+
+    TWITTER_USER = 'GuildWars'
+
+    def initialize(discord_bot, redis_client)
+      @discord_bot = discord_bot
+      @redis_client = redis_client
+      @feed = RSS::Parser.parse(ENV['TWITTER_FEED'])
+      @links_to_post = []
+      @channels = ENV['TWITTER_DISCORD_CHANNELS'].split(',')
+      @processing = false
+    end
+
+    def process
+      return if @processing
+      @processing = true
+      @feed.items.each do |item|
+        post_id = item.link.split('/').last.gsub('#m','')
+        link = "https://x.com/GuildWars/status/#{post_id}"
+        data = {
+          "id" => post_id,
+          "title" => item.title,
+          "link" => link,
+          "description" => item.description,
+          "pub_date" => item.pubDate.to_s,
+        }
+
+        existing_entry = @redis_client.call("EXISTS", post_id)
+        next if existing_entry == 1
+
+        @links_to_post << data
+      end
+
+      @channels.each do |channel|
+        @links_to_post.reverse.each do |data|
+          puts "Posting tweet #{data['link']} to #{channel}"
+          @discord_bot.send_message(channel, data['link'])
+          store_tweet(data)
+          sleep 2
+        end
+      end
+      @processing = false
+    end
+
+    def store_tweet(tweet)
+      @redis_client.call("HSET", tweet['id'], *tweet.flatten)
+    end
+
+  end
+end
